74290d7769e6b6c6de11e4b169993b240b61a51a
Mahmoud Ghanem <maahmoudghannem@gmail.com>
2026-06-19 14:35:25 +0300

    splash screen created

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/splash/presentation/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -10,7 +11,7 @@ class Bookly extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return const GetMaterialApp(
       home: SplashScreen(),
     );
   }
